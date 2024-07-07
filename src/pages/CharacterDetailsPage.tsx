ec226f4cf3642fa61a89eb3149b8f892f42f51fd
daniel <dschranc@gmail.com>
2024-07-07 23:30:14 +0200

    feat: Adjust character details page with usage of films service

--- src/pages/CharacterDetailsPage.tsx
+++ src/pages/CharacterDetailsPage.tsx
@@ -1,10 +1,20 @@
-import { Box, Button, Center, VStack } from "@chakra-ui/react";
-import { useQuery } from "@tanstack/react-query";
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  ListItem,
+  OrderedList,
+  VStack,
+} from "@chakra-ui/react";
+import { useQueries, useQuery } from "@tanstack/react-query";
 import { Link, useParams } from "react-router-dom";
 
 import { Spinner } from "../components/Spinner";
 import { PATHS } from "../router/Router";
 import * as charactersService from "../services/characters-service";
+import * as filmsService from "../services/films-service";
+import { trimIdFromUrl } from "../utils/url";
 
 type CharacterUrlParams = {
   id: string;
@@ -20,6 +30,17 @@ export function CharacterDetailsPage() {
     refetchOnWindowFocus: false,
   });
 
+  const films = useQueries({
+    queries: data
+      ? data.films
+          .map((film) => trimIdFromUrl(film))
+          .map((id) => ({
+            queryKey: ["film", id],
+            queryFn: async () => filmsService.getFilm(id),
+          }))
+      : [],
+  });
+
   if (isFetching || isLoading) {
     return <Spinner />;
   }
@@ -27,7 +48,14 @@ export function CharacterDetailsPage() {
   return (
     <Center>
       <VStack>
-        <Box>{data?.name}</Box>
+        <Heading size="lg">{data?.name}</Heading>
+        <Box>
+          <OrderedList>
+            {films.map((film) => (
+              <ListItem key={film.data?.created}>{film.data?.title}</ListItem>
+            ))}
+          </OrderedList>
+        </Box>
         <Box mt={30}>
           <Button as={Link} to={PATHS.home}>
             {"Back Home"}
