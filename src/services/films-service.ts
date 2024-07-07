75aadf2803745d1aaed864379da5ca6506c2f8a6
daniel <dschranc@gmail.com>
2024-07-07 23:29:49 +0200

    feat: Add service for films

---
+++ src/services/films-service.ts
@@ -0,0 +1,33 @@
+import { API_URL } from "../utils/constants";
+import { httpClient } from "../utils/http";
+
+const http = httpClient(API_URL);
+
+export type Film = {
+  title: string;
+  episode_id: number;
+  opening_crawl: string;
+  director: "George Lucas" | "Irvin Kershner" | "Richard Marquand" | "J. J. Abrams";
+  producer:
+    | "George Lucas"
+    | "Gary Kurtz"
+    | "Rick McCallum"
+    | "Howard G. Kazanjian"
+    | "Kathleen Kennedy"
+    | "Bryan Burk"
+    | "J. J. Abrams";
+  release_date: string;
+  characters: string[];
+  planets: string[];
+  starships: string[];
+  vehicles: string[];
+  species: string[];
+  created: string;
+  edited: string;
+  url: string;
+};
+
+export async function getFilm(id: string) {
+  const res = await http.get<Film>(`/films/${id}`);
+  return res.data;
+}
